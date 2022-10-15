3f72aa8090e28b9ee4adb65d8e8c588c051bde50
michaelmensa <michaelmensa@github.com>
2022-10-15 03:48:59 +0000

    two changes made in advanced tasks

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
@@ -8,22 +9,19 @@
 int main(void)
 {
 	int a, b;
-	
+
 	for (a = 0; a < 9; a++)
 	{
 		for (b = a + 1; b < 10; b++)
 		{
 			putchar((a % 10) + '0');
 			putchar((b % 10) + '0');
-			
 			if (a == 8 && b == 9)
 				continue;
-			
 			putchar(',');
 			putchar(' ');
 		}
 	}
-
 	putchar('\n');
 	return (0);
 }
