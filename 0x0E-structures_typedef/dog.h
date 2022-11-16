e06af8accd30e28c00316372626f5d4396d09db5
michaelmensa <michaelmensa@github.com>
2022-11-16 12:24:15 +0000

    struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+  * dog.h - struct my_dog contains
+  @name; dog's name
+  @age; dog's age
+  @owner: dog's owner
+  *
+  * Return: 0
+  */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
