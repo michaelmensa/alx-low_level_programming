c9c917338a2bfb728c2cdb23a55874d0d5988e94
michaelmensa <michaelmensa@github.com>
2022-11-16 12:29:31 +0000

    struct doG

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,17 +3,19 @@
 
 /**
   * dog.h - struct my_dog contains
-  @name; dog's name
-  @age; dog's age
-  @owner: dog's owner
+  * @name; dog's name
+  * @age; dog's age
+  * @owner: dog's owner
   *
   * Return: 0
   */
 
-struct dog
+struct dog /* user defined variable type */
 {
 	char *name;
 	float age;
 	char *owner;
+
+	/* description for struct dog */
 };
 #endif
