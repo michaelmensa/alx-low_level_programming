5cdb9532439f46f7a4535ea95fbe8f79760d22e0
michaelmensa <michaelmensa@github.com>
2022-11-18 14:06:04 +0000

    tasks, 1, and 3.

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,29 +1,29 @@
+#include <stdio.h>
 #include "variadic_functions.h"
 
 /**
- * print_numbers - prints numbers given as parameters
+ * print_numbers - function that prints numbers
  * @separator: string to be printed between numbers
- * @n: number of integers passed to the function
+ * @n: number of parameters
+ * Return: Sum of all parameters
  */
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
+	va_list arguments;
 	unsigned int i;
-	va_list list;
+	int aux = 0;
 
-	va_start(list, n);
 
+	va_start(arguments, n);
 	for (i = 0; i < n; i++)
 	{
-		if (!separator)
-			printf("%d", va_arg(list, int));
-		else if (separator && i == 0)
-			printf("%d", va_arg(list, int));
+		aux = va_arg(arguments, int);
+		if (i < (n - 1) && separator != 0)
+			printf("%d%s", aux, separator);
 		else
-			printf("%s%d", separator, va_arg(list, int));
+			printf("%d", aux);
 	}
-
-	va_end(list);
-
+	va_end(arguments);
 	printf("\n");
 }
--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,47 +1,97 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "variadic_functions.h"
 
 /**
- * print_all - prints anything
- * @format: list of types of arguments passed to the function
+ * print_i - prints int
+ * @list: arguement of list
+ * @s: seperator
+ * Return: none
+ */
+
+void print_i(va_list list, char *s)
+{
+	printf("%s%d", s, va_arg(list, int));
+}
+
+/**
+ * print_c - prints char
+ * @list: arguement char
+ * @sep: seperator
+ */
+
+void print_c(va_list list, char *sep)
+{
+	printf("%s%c", sep, va_arg(list, int));
+}
+
+/**
+ * print_s - prints string
+ * @sep: seperator
+ * @list: list to print
+ * Return: none
+ */
+
+void print_s(va_list list, char *sep)
+{
+	char *s;
+
+	s = va_arg(list, char *);
+	if (s == NULL)
+		s = "(nil)";
+	printf("%s%s", sep, s);
+}
+
+/**
+ * print_f - prints floats
+ * @sep: float to print
+ * @list: next arguement of list to print
+ * Return: none
+ */
+
+void print_f(va_list list, char *sep)
+{
+	printf("%s%f", sep, va_arg(list, double));
+}
+
+/**
+ * print_all -  function that prints anything
+ * @format:  list of types of arguments passed to the function
+ * Return: nothing
  */
 
 void print_all(const char * const format, ...)
 {
-	int i = 0;
-	char *str, *sep = "";
 	va_list list;
+	int i, j;
+	char *separator;
+
+	type_t ops[] = {
+		{"c", print_c},
+		{"i", print_i},
+		{"f", print_f},
+		{"s", print_s},
+		{NULL, NULL}
+	};
 
 	va_start(list, format);
-	if (format)
+	i = 0;
+	separator = "";
+	while (format != NULL && format[i] != '\0')
 	{
-		while (format[i])
+		j = 0;
+		while (j < 4)
 		{
-			switch (format[i])
+			if (format[i] == *(ops[j]).op)
 			{
-				case 'c':
-					printf("%s%c", sep, va_arg(list, int));
-					break;
-				case 'i':
-					printf("%s%d", sep, va_arg(list, int));
-					break;
-				case 'f':
-					printf("%s%f", sep, va_arg(list, double));
-					break;
-				case 's':
-					str = va_arg(list, char *);
-					if (!str)
-						str = "(nil)";
-					printf("%s%s", sep, str);
-					break;
-				default:
-					i++;
-					continue;
-			}
-			sep = ", ";
-			i++;
-		}
-	}
+				ops[j].f(list, separator);
+				separator = ", ";
 
+			}
+			j++;
+		}
+		i++;
+	}
 	printf("\n");
 	va_end(list);
 }
