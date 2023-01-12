bb42e8034902e0427784f1d6ab020b3eddb14832
michaelmensa <michaelmensa@github.com>
2023-01-12 11:41:50 +0000

    task 1 commit

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+  * dlistint_len - returns the number of elements in a 
+  * doubly linked list_t list
+  * @h: pointer
+  * Return: number of elements
+  */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t cont = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		cont++;
+	}
+	return cont;
+}
