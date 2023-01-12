5419ceb92da1dd2b1a0c2747097b92942dbc3837
michaelmensa <michaelmensa@github.com>
2023-01-12 12:21:34 +0000

    task 5 commit

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+  * get_dnodeint_at_index - returns the nth node of dlistint_t list
+  * @head: input header pointer
+  * @index: node index starting from 0
+  * Return: NULL if node does not exist
+  */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *node;
+	unsigned int cont = 0;
+
+	node = head;
+	while (node != NULL && cont <= index)
+	{
+		if (cont == index)
+			return node;
+		node = node->next;
+		cont++;
+	}
+	return NULL;
+}
