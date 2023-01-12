00f15302fe83c11e9d65ed4059f537f6a0a43691
michaelmensa <michaelmensa@github.com>
2023-01-12 12:28:53 +0000

    task 6 commit

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+  * sum_dlistint - returns the sum of all data n, of dlistint
+  * @head: head of linked list
+  * Return: sum of all data n, in linked list, 0 if empty
+  */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	if (head == NULL)
+		return 0;
+	while (head != NULL)
+	{
+		sum += head->n;
+		head =head->next;
+	}
+	return sum;
+}
